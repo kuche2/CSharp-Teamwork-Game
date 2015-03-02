6a3ab71d2d36a27b71c00cc93b6dd96ddbe4b0bb
igergow <ivo.gergov@gmail.com>
2015-03-02 13:12:12 +0200

    New Class for (Hero class)

--- GameProjects/AirplainShooterNext/AirplaneNext.cs
+++ GameProjects/AirplainShooterNext/AirplaneNext.cs
@@ -11,51 +11,7 @@ namespace AirplaneShooterNext
         public static int Height = 55;
         public static int Width = 120;
     }
-    public class Hero
-    {
-        public static void UserAiplainKeysOptions()
-        {
-            // user keys : "<" , ">" , "space" - shooting
-            while (Console.KeyAvailable)
-            {
-                ConsoleKeyInfo userInput = Console.ReadKey();
-                if (userInput.Key == ConsoleKey.LeftArrow)
-                {
-                    AirplaneNext.currentAirplainPosX--;
-                    AirplaneNext.AirplainMovingLimits();
-                    Console.SetCursorPosition(AirplaneNext.currentAirplainPosX + 7, AirplaneNext.currentAirplainPosY + 2);
-                    Console.Write(' ');
-                    AirplaneNext.DrawFigureAtPosition(AirplaneNext.currentAirplainPosX, AirplaneNext.currentAirplainPosY, ConsoleColor.DarkGreen, AirplaneNext.airplain);
-                }
-                if (userInput.Key == ConsoleKey.RightArrow)
-                {
-                    AirplaneNext.currentAirplainPosX++;
-                    AirplaneNext.AirplainMovingLimits();
-                    Console.SetCursorPosition(AirplaneNext.currentAirplainPosX - 1, AirplaneNext.currentAirplainPosY + 2);
-                    Console.Write(' ');
-                    AirplaneNext.DrawFigureAtPosition(AirplaneNext.currentAirplainPosX, AirplaneNext.currentAirplainPosY, ConsoleColor.DarkGreen, AirplaneNext.airplain);
-                }
-
-                if (userInput.Key == ConsoleKey.Spacebar)
-                {
-                    AirplaneNext.Bullet bullet = new AirplaneNext.Bullet(AirplaneNext.currentAirplainPosX + 3, AirplaneNext.currentAirplainPosY + 1, ConsoleColor.Magenta, '^');
-                    AirplaneNext.bullets.Add(bullet);
-                    Audio.shot();
-                }
-
-                if (userInput.Key == ConsoleKey.P)
-                {
-                    while (true)
-                    {
-                        if (Console.ReadKey().Key == ConsoleKey.P)
-                        {
-                            break;
-                        }
-                    }
-                }
-            }
-        }
-    }
+    
 
     class AirplaneNext
     {
